81f9dd193a9ca60535ef77d9b0f8436075cbd0d3
lethanhkim <thanhkim1997.pnv@gmail.com>
2026-06-18 11:09:32 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
